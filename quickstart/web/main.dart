11b9847a0caf08cc8f50ac48bdd68b504e62deb8
Tuyen Dang <tuyendq@gmail.com>
2022-10-30 20:24:58 +0700

    add thingsTodo() function

--- quickstart/web/main.dart
+++ quickstart/web/main.dart
@@ -1,5 +1,18 @@
 import 'dart:html';
 
+Iterable<String> thingsTodo() sync* {
+  const actions = ['Walk', 'Wash', 'Feed'];
+  const pets = ['cats', 'dogs'];
+
+  for (final action in actions) {
+    for (final pet in pets) {
+      if (pet != 'cats' || action == 'Feed') {
+        yield '$action the $pet';
+      }
+    }
+  }
+}
+
 void main() {
   querySelector('#output')?.text = 'Your Dart app is running.';
 }
